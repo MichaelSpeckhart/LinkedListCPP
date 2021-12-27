002fa9fbcfef8c55e81d3f2057ebdaaa81775160
Michael Speckhart <msspeck394@gmail.com>
2021-12-27 16:31:51 -0500

    Adding the LinkedList class and Node struct

--- includes/LinkedList.h
+++ includes/LinkedList.h
@@ -1,12 +1,31 @@
-#ifndef LinkedList_h
-#define LinkedList_h
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 
+#ifndef LINKEDLIST_h
+#define LINKEDLIST_h
+
+//***************
+// Creating Node struct that will contain value as an integer
+// and a pointer to the next value in the LinkedList.S
+//***************
 struct Node {
     int value;
     Node *next;
 };
 
-
+class LinkedList {
+    private:
+        Node head;
+        size_t length;
+    
+    public:
+        LinkedList();
+        bool insertFront();
+        bool insertLast();
+        bool insertAtPosition();
+        void display();
+};
 
 
 
